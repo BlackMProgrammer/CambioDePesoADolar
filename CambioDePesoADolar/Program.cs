bb43c5f2b1815bd5daadd7afca0177f8cbb64555
Cristhian De Castro <cristhiancastro01@gmail.com>
2020-08-31 20:21:54 -0400

    Agregar archivos de proyecto.

---
+++ CambioDePesoADolar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CambioDePesoADolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte Un numero para convertir a Dolar");
+            string valor = Console.ReadLine();
+            int pesoDom = Convert.ToInt32(valor);
+
+            Console.WriteLine("El numero que Ingreso fue: {0}", pesoDom);
+
+            Console.WriteLine("El Dolar esta a RD$ 58.50 ");
+
+            Console.WriteLine("Su monto en dolares es de {0}", pesoDom * 58.50);
+        }
+    }
+}
